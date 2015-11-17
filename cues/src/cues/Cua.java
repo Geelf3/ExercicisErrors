4104d8347c5d2d2ad4ccbe72461f6c9bb0d1089d
Gerard Sala <gerard.gee.2@gmail.com>
2015-11-17 17:19:41 +0100

    Exercici de cues amb try/catch

---
+++ cues/src/cues/Cua.java
@@ -0,0 +1,12 @@
+package cues;
+
+
+	
+	public interface Cua {
+	    void encuar(Object item);
+	    Object desencuar();
+	    Object primer();
+	    int mida();
+	    boolean esBuida();
+	    void eliminar();
+	}
